58cf1d25d30ee27178a34973d606316f41928faa
rudensergey <rudensergey@gmail.com>
2020-06-19 14:17:09 +0300

    add matrix structure and some styling

--- src/components/extendlayer/Extendlayer.js
+++ src/components/extendlayer/Extendlayer.js
@@ -1,6 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Game } from "../library";
+import "./extendlayer.css";
 
+/**
+ * ExtendLayer - represent layer which calculate the screen width, heigth and push it to the props
+ * @return {Component}
+ */
 export const ExtendLayer = () => {
-    return <Game initialHeigth={5} initialWidth={5} />;
+    // const [width, setWidth] = useState(window.innerWidth);
+    // const [height, setHeight] = useState(window.innerHeight);
+
+    // useEffect(() => {
+    //     const HandleresizeWidth = () => {
+    //         setWidth(window.innerWidth);
+    //         console.log(width);
+    //     };
+    //     window.addEventListener("resize", HandleresizeWidth);
+    //     console.log(width);
+    // });
+
+    // useEffect(() => {
+    //     const HandleresizeHeight = () => {
+    //         setHeight(window.innerHeight);
+    //     };
+    //     window.addEventListener("resize", HandleresizeHeight);
+    // });
+
+    return (
+        <div className="extendlayer">
+            <Game initialHeight={10} initialWidth={7} />
+        </div>
+    );
 };
--- src/components/game/Game.js
+++ src/components/game/Game.js
@@ -1,27 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
 import "./game.css";
 
+/**
+ *
+ * @param {props} props - includes:
+ * @param {number} initialHeight - amount rows in this table
+ * @param {number} initialWidth - amount columns in this table
+ *
+ * @return {HTMLElement}
+ */
 export const Game = (props) => {
-    const { initialHeigth, initialWidth } = props;
+    const { initialHeight, initialWidth } = props;
 
+    /**
+     * Create matrix - creates new matrix based on the input
+     *
+     * @param {number} h - pepresents height of the matrix
+     * @param {number} w - represent width of the matrix
+     * @return {Array}
+     */
     const createMatrix = (h, w) =>
-        new Array(h).fill().map(() => new Array(w).fill(""));
+        new Array(h).fill().map(() => new Array(w).fill(["", ""]));
+
+    console.log(createMatrix(initialHeight, initialWidth));
 
     return (
         <div
             className="game"
             style={{
-                gridTemplateColumns: `repeat(${initialHeigth}, 1fr)`,
-                gridTemplateRows: `repeat(${initialWidth}, 1fr)`,
+                gridTemplateColumns: `repeat(${initialWidth}, 100px)`,
+                gridTemplateRows: `repeat(${initialHeight}, 100px)`,
             }}
         >
-            {createMatrix(initialHeigth, initialWidth).map((a) =>
-                a.map((b) => (
-                    <div className="game__cell">
-                        <p>Cell</p>
-                    </div>
+            {createMatrix(initialHeight, initialWidth).map((a, aIndex) =>
+                a.map((b, bIndex) => (
+                    <Cell
+                        coords={{ aIndex, bIndex }}
+                        key={aIndex + bIndex}
+                        status={b[1]}
+                    />
                 ))
             )}
+        </div>
+    );
+};
+
+/**
+ * Cell - represents one cell of the table
+ *
+ * @param {props} props - includes:
+ * @param {object} coords - coordinates of that cell in the matrix
+ * @param {string | boolean} status - current value of this cell
+ * @return {HTMLElement}
+ */
+const Cell = (props) => {
+    const {
+        status,
+        coords: { aIndex: x, bIndex: y },
+    } = props;
+
+    const [stat, setStat] = useState(status);
+    let name;
+    let classNames;
+
+    switch (stat) {
+        case true:
+            name = "O";
+            classNames = "game__cell-tac";
+            break;
+        case false:
+            name = "X";
+            classNames = "game__cell-toe";
+            break;
+        case "success":
+            name = "";
+            classNames = "game__cell-success";
+            break;
+    }
+
+    return (
+        <div
+            className={"game__cell " + classNames}
+            key={x + y}
+            onClick={() => {
+                stat === true || stat === false
+                    ? alert("Хватит меня нажимать!")
+                    : setStat(false);
+            }}
+        >
+            <p>{name}</p>
         </div>
     );
 };
